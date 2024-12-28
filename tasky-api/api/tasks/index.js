bcbee5542ca3ff12c77a764b5e159ab980157a82
Jiyun Kwon <20087623@mail.wit.ie>
2024-12-28 15:51:09 +0000

    Mongo lab Users Router

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -10,15 +10,10 @@ router.get('/', (req, res) => {
     res.json(tasksData);
 });
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    const task = tasksData.tasks.find(task => task.id === id);
-    if (!task) {
-        return res.status(404).json({ status: 404, message: 'Task not found' });
-    }
-    return res.status(200).json(task);
+router.get('/', async (req, res) => {
+    const tasks = await Task.find().populate('userId', 'username');
+    res.status(200).json(tasks);
 });
-
 router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
